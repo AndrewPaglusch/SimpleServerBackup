88045affa3d7718d0cd7e1d3c9c34f2a4e550ff6
josh nave <jnave@nhjump.nave.house>
2022-07-17 11:37:53 -0500

    adding args, moving parsing

--- lib/Server.py
+++ lib/Server.py
@@ -9,8 +9,9 @@ Start is the main method available to the user.
 __author__ = "Andrew Paglusch"
 __license__ = "MIT"
 
-class Server:
-    def __init__(self, logging, server_host, server_config):
+class Backup:
+    def __init__(self, logging, server_host, server_config, logfile='TBD'):
+        self.starttime = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
         self.logging = logging
         self.host = server_host # web01.whatever.com
         self.username = server_config['username'] # root
@@ -18,23 +19,27 @@ class Server:
         self.port = server_config['port'] # 22
         self.ssh_args = ' '.join(server_config['ssh_args'])
         self.remote_path = server_config['remote_path'] # /
-        self.excludes = server_config['excludes']
+        self.excludes = [ f"--exclude={ex}" for ex in server_config['excludes'] ]
         self.backup_dest = 'backups/' + server_host + '/' # backups/web01.whatever.com/
-        self.logfile = "Not yet set" # will be set by log_backup_results
-
+        self.logfile = f"logs/{self.host}_{self.starttime}.log" if logfile == 'TBD' else logfile
+        self.rsync_cmd = ['rsync', '-e', f'ssh -p {self.port} {self.ssh_args}', '-avPHS', '--delete'] + self.excludes
         # will be updated to True after pre/post scripts have been synced to host
         self.script_deployed = False
 
     def deploy_scripts(self, scripts):
-        try:
-            self.run_remote_command('mkdir /tmp/SimpleServerBackup_scripts')
-        except Exception as ex:
-            return (False, f"Error while creating remote script directory: {ex}")
+        pass
 
-        rsync_command = ['rsync', '-e', f'ssh -p {self.port} {self.ssh_args}'] + \
-                ['-avPH'] + \
-                ['--delete', f"{self.connect_string}:{self.remote_path}", self.backup_dest ]
-        rsync_exec = subprocess.run(rsync_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    def __run_rsync__(self, remote_dest, local_dest, direction='from_remote'):
+        rsync_cmd = self.rsync_cmd
+        if direction == 'from_remote':
+            rsync_cmd.append(f"{self.connect_string}:{remote_dest}")
+            rsync_cmd.append(local_dest)
+        else:
+            rsync_cmd.append(local_dest)
+            rsync_cmd.append(f"{self.connect_string}:{remote_dest}")
+        rsync_exec = subprocess.run(rsync_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        return({ 'return_code': rsync_exec.returncode,
+             'output': rsync_exec.stdout.decode("utf-8") or b"" })
 
     def run_pre_scripts(self):
         pass
@@ -45,7 +50,7 @@ class Server:
     def clean_scripts(self):
         pass
 
-    def run_remote_command(self, command):
+    def run_single_remote_command(self, command):
         ssh_command = ['ssh', '-p', self.port, self.connect_string, command]
         cmd = subprocess.run(ssh_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = cmd.stdout or b""
@@ -53,41 +58,23 @@ class Server:
             raise Exception(f"Remote command resulted in a non-zero return code: {cmd.returncode}. Command output: {output}")
         return output.decode("utf-8")
 
-    def run_rsync(self):
-        self.excludes = [ f"--exclude={ex}" for ex in self.excludes ]
-        ## maybe move rsync to its own method
-        rsync_command = ['rsync', '-e', f'ssh -p {self.port} {self.ssh_args}'] + \
-                        ['-avPH'] + \
-                        self.excludes + \
-                        ['--delete', f"{self.connect_string}:{self.remote_path}", self.backup_dest ]
-        rsync_exec = subprocess.run(rsync_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-        return_code = rsync_exec.returncode
-        rsync_output = rsync_exec.stdout or b""
-
-        return({ 'return_code': return_code,
-             'output': rsync_output.decode("utf-8") })
-
     def log_backup_results(self, output):
-        self.logfile = f"logs/{self.host}_{self.starttime}.log"
-
         self.logging.info(f"Saving rsync output for {self.host} to {self.logfile}")
         f = open(self.logfile, 'w')
         f.write(output)
         self.logging.debug(f"Finished saving rsync output for {self.host} to {self.logfile}")
 
     def start(self):
-        self.starttime = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
         self.logging.info(f"Backup started for {self.host}")
 
         # deploy pre/post scripts
-        # deploy_scripts()
+        #self.deploy_scripts()
 
         # run pre-scripts
         # run_pre_scripts()
 
         # run the backup
-        rsync_results = self.run_rsync()
+        rsync_results = self.__run_rsync__(remote_dest=self.remote_path, local_dest=self.backup_dest)
         self.log_backup_results(rsync_results['output'])
 
         # TODO: Simply checking the return code of rsync
--- run.py
+++ run.py
@@ -1,64 +1,27 @@
 #!/usr/bin/env python3
 import sys
-from pathlib import Path
 from concurrent import futures
-from configparser import ConfigParser
-from pprint import pprint
 import subprocess
 import logging
 import datetime
-from lib.Server import Server
-def load_main_config():
-    """load main config from disk"""
-    cp = ConfigParser()
-    cp.read("config.ini")
-    config = {
-        'concurrency': int(cp.get('main', 'concurrency', fallback=1))
-    }
-    return config
-
-def load_all_server_config():
-    """load server configs from disk"""
-    config_files = Path('./servers.d').glob('*.ini')
-    server_configs = {}
-    for config_file in config_files:
-        logging.debug(f"Loading config file {config_file} from disk")
-        config = ConfigParser()
-        config.read(config_file)
-
-        host = config.get('connection', 'host')
-        server_configs[host] = {
-            'port': config.get('connection', 'port', fallback="22"),
-            'username': config.get('connection', 'username', fallback="root"),
-            'ssh_args': [ ssh_arg.strip() for ssh_arg in config.get('connection', 'ssh_args', fallback="").split(',') if ssh_arg.strip() != '' ],
-            'remote_path': config.get('main', 'remote_path'),
-            'excludes': [ exclude.strip() for exclude in config.get('main', 'excludes').split(',') if exclude.strip() != '' ]
-        }
-    return server_configs
+from lib.Server import Backup
+from lib.ssbArgs import ssbArgs
+from lib.Parseconfig import ssbConfig
 
 def main():
     logging.basicConfig(stream=sys.stdout,
         level=logging.INFO,
         format='%(asctime)s (%(levelname)s) - %(message)s',
         datefmt='%Y-%m-%dT%H:%M:%SZ')
-    try:
-        config = load_main_config()
-    except Exception:
-        logging.exception(f"Unable to read main config from disk.")
-        exit()
-
-    try:
-        server_configs = load_all_server_config()
-    except Exception:
-        logging.exception(f"Unable to load server configs from disk.")
-        exit()
+    args   = ssbArgs(logging).args
+    config = ssbConfig(args.config, logging).config
 
     # build and start thread pool
     with futures.ThreadPoolExecutor(max_workers=config['concurrency']) as ex:
         backup_futures = {}
-        for server_host, server_config in server_configs.items():
+        for server_host, server_config in config['server_configs'].items():
             logging.debug(f"Adding {server_host} to thread pool")
-            s = Server(logging, server_host, server_config)
+            s = Backup(logging, server_host, server_config)
             backup_futures[ex.submit(s.start)] = server_host
 
         for future in futures.as_completed(backup_futures):
