8481766d3706e35fd105937480a08388cfdc449c
josh nave <jnave@nhjump.nave.house>
2022-07-18 19:02:14 -0500

    clean up pre/post method

--- lib/Parseconfig.py
+++ lib/Parseconfig.py
@@ -20,8 +20,8 @@ class SSBConfig:
             'concurrency':       self.cp.getint('main', 'concurrency', fallback=1),
             'scripts_directory': self.cp.get('main', 'scripts_dir'),
             'server_directory':  self.cp.get('main', 'server_dir'),
-            'server_configs': {},
-            'scripts_configs': {}
+            'server_configs':    {},
+            'scripts_configs':   {}
         }
         return config
 
@@ -36,7 +36,7 @@ class SSBConfig:
                'remote_path': sc.get('main', 'remote_path'),
                'excludes': [ exclude.strip() for exclude in sc.get('main', 'excludes').split(',') if exclude.strip() != '' ],
                'scripts_location':  sc.get('main', 'scripts_remote_location', fallback="/tmp")
-               }
+            }
         except Exception:
             self.logging.error(f"Unable to load {filepath} due to {Exception}")
 
@@ -52,20 +52,15 @@ class SSBConfig:
         for server in script_servers:
             if not self._verify_script_to_server(server):
                 continue
-            files = []
-            for file in server.iterdir():
-                files.append(file.parts[-1])
+            files = [ file.parts[-1] for file in server.iterdir() ]
             pre, post = self._sort_files(files)
             self.config['scripts_configs'][server.parts[-1]] = {}
             self.config['scripts_configs'][server.parts[-1]]['pre'] = pre
             self.config['scripts_configs'][server.parts[-1]]['post'] = post
 
     def _sort_files(self, files):
-        pre, post = [], []
-        [ pre.append(file) for file in files if file.startswith('pre') ]
-        [ post.append(file) for file in files if file.startswith('post') ]
-        pre.sort()
-        post.sort()
+        pre  = sorted([ file for file in files if file.startswith('pre') ])
+        post = sorted([ file for file in files if file.startswith('post') ])
         return pre, post
 
     def _verify_script_to_server(self, server):
@@ -74,7 +69,6 @@ class SSBConfig:
             return False
         hostname = server.parts[-1]
         if hostname not in self.config['server_configs'].keys():
-            self.logging.info(f"{hostname} is does not have a config, skipping")
+            self.logging.info(f"{hostname} has no scripts - no pre/post scripts will be ran.")
             return False
         return True
-
